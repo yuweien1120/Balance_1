5d0dd834b04aaf2f7fabe5f8e2375a409107b2d1
杨浩淼 <2345326388@qq.com>
2020-11-12 19:31:35 +0800

    合并main更改

--- HITSIC_MK66F18_MCUX/source/main.cpp
+++ HITSIC_MK66F18_MCUX/source/main.cpp
@@ -130,36 +130,73 @@ void main(void)
     pitMgr_t::init();
     /** 初始化I/O中断管理器 */
     extInt_t::init();
+    /** 初始化OLED屏幕 */
+    DISP_SSD1306_Init();
+    //extern const uint8_t DISP_image_100thAnniversary[8][128];
+    //DISP_SSD1306_BufferUpload((uint8_t*) DISP_image_100thAnniversary);
+    uint8_t DISP_image_R[8][128];
+    for(int i=0;i<8;i++)
+    {
+        for(int j=0;j<128;j++)
+        {
+            if((i==4||i==3||i==2)&&j>40&&j<100)
+                DISP_image_R[i][j]=0xFF;
+            else
+                DISP_image_R[i][j]=0x00;
+        }
+    }
+    DISP_SSD1306_BufferUpload((uint8_t*)DISP_image_R);
     /** 初始化菜单 */
-    MENU_Init();
-    MENU_Data_NvmReadRegionConfig();
-    MENU_Data_NvmRead(menu_currRegionNum);
+    //MENU_Init();
+    //MENU_Data_NvmReadRegionConfig();
+    //MENU_Data_NvmRead(menu_currRegionNum);
     /** 菜单挂起 */
-    MENU_Suspend();
+    //MENU_Suspend();
     /** 初始化摄像头 */
     //TODO: 在这里初始化摄像头
     /** 初始化IMU */
     //TODO: 在这里初始化IMU（MPU6050）
     /** 菜单就绪 */
-    MENU_Resume();
+    //MENU_Resume();
     /** 控制环初始化 */
     //TODO: 在这里初始化控制环
     /** 初始化结束，开启总中断 */
     HAL_ExitCritical();
 
     /** 内置DSP函数测试 */
+    pitMgr_t::insert(1000, 23, pit_test,pitMgr_t::enable);
+    /** 初始化结束，开启总中断 */
+    HAL_ExitCritical();
     float f = arm_sin_f32(0.6f);
 
+    SCFTM_PWM_Change(FTM0,0,20000,60);
+    SCFTM_PWM_Change(FTM1,1,20000,60);//驱动电机
     while (true)
     {
         //TODO: 在这里添加车模保护代码
     }
 }
 
+void pit_test(void*a)
+{
+    GPIO_PortToggle(GPIOC,1U<<0);
+}
 void MENU_DataSetUp(void)
 {
     MENU_ListInsert(menu_menuRoot, MENU_ItemConstruct(nullType, NULL, "EXAMPLE", 0, 0));
     //TODO: 在这里添加子菜单和菜单项
+    static int32_t region_a = 4096;
+    static float region_pi = 3.141;
+    static menu_list_t *testList;
+    testList = MENU_ListConstruct("TestList", 20, menu_menuRoot);
+    assert(testList);
+    MENU_ListInsert(menu_menuRoot, MENU_ItemConstruct(menuType, testList, "FIRST TRY", 0, 0));
+    {
+        MENU_ListInsert(testList, MENU_ItemConstruct(variType, &region_a, "region_i", 1, menuItem_data_region));
+        MENU_ListInsert(testList, MENU_ItemConstruct(varfType, &region_pi, "region_f", 2, menuItem_data_region));
+    }
+
+    //sc::SC_UnitTest_AutoRefreshAddMenu(menu_menuRoot);
 }
 
 void CAM_ZF9V034_DmaCallback(edma_handle_t *handle, void *userData, bool transferDone, uint32_t tcds)
